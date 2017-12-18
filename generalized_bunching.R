32fd13fdd35650b74b62de83e76abc32d3702e8c
Blake <g14n14ri12@gmail.com>
2017-12-18 14:09:57 +1000

    new H, still wrong?

--- generalized_bunching.R
+++ generalized_bunching.R
@@ -1,23 +1,29 @@
 library(BosonSampling)
 set.seed(2345)
 
-bosons <- 5L
-modes <- 13L
+bosons <- 4L
+test <- 2L
+modes <- 8L
+bunch_bin <- modes - test
 sample_num <- 500L
-bunch_bin <- 10L
-#
 
 network_unitary <- randomUnitary(modes)
 
-H <- network_unitary[1:bunch_bin, 1:bunch_bin]
+H <- matrix(data = 0, nrow = bunch_bin, ncol = bunch_bin)
+for (i in 1:bunch_bin) {
+  for (j in 1:bunch_bin) {
+    for (l in 1:bunch_bin) {
+      H[i, j] <- H[i, j] + (network_unitary[i,l] * Conj(network_unitary[j,l]))
+    }
+  }
+}
 p_b <- Mod(cxPerm(H))
 
 sampling_matrix <- network_unitary[, 1:bosons]
 outcomes <- bosonSampler(sampling_matrix, sample_num)$values > bunch_bin
-
 unbunched <- vector(mode = "logical", length = sample_num)
 for (i in 1:sample_num) {
   unbunched[i] <- TRUE %in% outcomes[, i]
 }
-
 result <- 1 - mean(unbunched)
+
